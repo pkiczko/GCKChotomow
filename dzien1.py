de794e2879994a68949b4df9227660b91289f75f
Pawel Kiczko <pkiczko@gmail.com>
2022-02-15 17:43:14 +0100

    poczatek

---
+++ dzien1.py
@@ -0,0 +1,30 @@
+import math #importuje biblioteke matematyczną
+
+zmienna = "Hello world" #przypisujemy wartość do zmiennej "zmienna"
+liczba = 456,60 #definiujemy liczbę
+liczba_calkowita = 6
+ilosc_jablek = 4
+prawda_fałsz = True #może mieć wartość True albo False (tak/nie)
+
+lista = [1,2,3,4,5]
+
+słownik = {"Adam":227677300, "Beata":"zielony"}
+
+print('Hello, World!')
+print(zmienna)
+print(prawda_fałsz)
+print(zmienna + "5") #łączenie dwóch tekstów
+
+#for i in range(1,10): #powtórz zawartość 10 razy
+    #test
+    #test2
+#brak wcięcia - koniec pętli
+
+print(1)
+
+x=1
+y=2
+if (x > y):
+    print("X większe od y!")
+else:
+    print("X mniejsze od y!")
