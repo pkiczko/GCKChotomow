b963371f6539468483681bad2be9eca8b3fb70cf
Pawel Kiczko <pkiczko@gmail.com>
2022-02-22 16:49:18 +0100

    dzien2

---
+++ dzien2.py
@@ -0,0 +1,22 @@
+#materiały: https://github.com/Asabeneh/30-Days-Of-Python/blob/master/03_Day_Operators/03_operators.md
+
+x = 5
+y = '123 test @@@'
+
+print(x)
+print(y)
+print('y')
+#print(z) #błąd - z nie jest zdefiniowane
+
+#x = x + 3
+x += 3 #do x dodajemy 3 - więc x będzie równe 8
+
+print("Nowa wartość x: ", x)
+
+#x %= 3
+z = x % 3 #również równe 2 - reszta z dzielenia 8 przez 3
+
+print("Reszta z dzielenia x przez 3: ", z )
+
+print(x // 3) 
+# 2 ** 3 = 2*2*2
