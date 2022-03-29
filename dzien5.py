d7eb4ecf87c1f965af2e4d4cd58795ce8dedb1dd
Pawel Kiczko <pkiczko@gmail.com>
2022-03-29 16:33:24 +0200

    up

--- dzien5.py
+++ dzien5.py
@@ -29,3 +29,48 @@ suma = 0
 for liczba in zbior_A:
     suma += liczba
 print('Suma liczb ze zbioru A to: ', suma)
+
+#ze strony https://github.com/Asabeneh/30-Days-Of-Python/blob/master/10_Day_Loops/10_loops.md
+person = {      
+    'first_name':'Asabeneh',    #biblioteka(dictionary) zawiera zestawy klucz:wartość
+    'last_name':'Yetayeh',      #wartość może też być zbiorem, biblioteką etc.
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'], 
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+}
+for key in person:
+    print(key)
+
+for key, value in person.items(): #dzięki tej operacji wyciągniemy z biblioteki person (dictionary) zarówno klucze jak i wartości
+    print(key, ':' , value)
+
+#Napisz funkcję używając pętli by uzyskać następujący wynik:
+  #
+  ##
+  ###
+  ####
+  #####
+  ######
+  #######
+#podpowiedź
+symbol = '#'
+symbol += '#'
+print(len(symbol)) #długość '##' to 2
+print('\t1\t2\t3')
+czubek = '  #'
+srodek = ' ###'
+dol='#####'
+print(czubek)
+print(srodek)
+print(dol)
+
+spacja = ' '
+symbol_hash = "#"
+for k in range(5):
+    print((5-k)*spacja, k*2*symbol_hash)
+
